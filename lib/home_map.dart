c745149a1f99ab383c537dc7b6f9a5f89551a221
DanielBarragan96 <ie702554@iteso.mx>
2020-10-25 12:30:05 -0600

    implementation of search using text, and added button to move to current position

--- lib/home_map.dart
+++ lib/home_map.dart
@@ -1,4 +1,8 @@
+import 'dart:async';
+
+import 'package:address_search_field/address_search_field.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
@@ -10,6 +14,8 @@ class HomeMap extends StatefulWidget {
 }
 
 class _HomeMapState extends State<HomeMap> {
+  final Completer<GoogleMapController> _googleMapController =
+      Completer<GoogleMapController>();
   Set<Marker> _mapMarkers = Set();
   GoogleMapController _mapController;
   Position _currentPosition;
@@ -17,29 +23,68 @@ class _HomeMapState extends State<HomeMap> {
     longitude: 20.608148,
     latitude: -103.417576,
   );
+  bool _currentPositionCamera = true;
 
   @override
   Widget build(BuildContext context) {
     return FutureBuilder(
       future: _getCurrentPosition(),
       builder: (context, result) {
+        final Size size = MediaQuery.of(context).size;
         if (result.error == null) {
           if (_currentPosition == null) _currentPosition = _defaultPosition;
           return Scaffold(
-            body: Stack(
-              children: <Widget>[
-                GoogleMap(
-                  onMapCreated: _onMapCreated,
-                  markers: _mapMarkers,
-                  onLongPress: _setMarker,
-                  initialCameraPosition: CameraPosition(
-                    target: LatLng(
-                      _currentPosition.latitude,
-                      _currentPosition.longitude,
+            body: SafeArea(
+              child: Stack(
+                children: <Widget>[
+                  GoogleMap(
+                    onMapCreated: _onMapCreated,
+                    markers: _mapMarkers,
+                    onLongPress: _setMarker,
+                    initialCameraPosition: CameraPosition(
+                      target: LatLng(
+                        _currentPosition.latitude,
+                        _currentPosition.longitude,
+                      ),
                     ),
                   ),
-                )
-              ],
+                  Positioned(
+                    top: 20.0,
+                    left: size.width * 0.1,
+                    right: size.width * 0.1,
+                    child: Container(
+                      width: size.width * 0.8,
+                      color: Colors.white,
+                      child: AddressSearchField(
+                        country: "Mexico",
+                        hintText: "Address",
+                        noResultsText: "No results.",
+                        onDone: (BuildContext dialogContext,
+                            AddressPoint point) async {
+                          if (point.found) {
+                            _setMarker(LatLng(point.latitude, point.longitude));
+                            Navigator.of(context).pop();
+                          }
+                        },
+                      ),
+                    ),
+                  ),
+                  Positioned(
+                    bottom: 30,
+                    left: 5,
+                    child: IconButton(
+                        icon: Icon(
+                          Icons.my_location,
+                          color: Colors.blue,
+                          size: 40,
+                        ),
+                        onPressed: () {
+                          _currentPositionCamera = true;
+                          _getCurrentPosition();
+                        }),
+                  ),
+                ],
+              ),
             ),
           );
         } else {
@@ -81,6 +126,17 @@ class _HomeMapState extends State<HomeMap> {
         ),
       );
     });
+
+    _currentPositionCamera = false;
+    // move camera
+    _mapController.animateCamera(
+      CameraUpdate.newCameraPosition(
+        CameraPosition(
+          target: coord,
+          zoom: 15.0,
+        ),
+      ),
+    );
   }
 
   Future<void> _getCurrentPosition() async {
@@ -107,17 +163,19 @@ class _HomeMapState extends State<HomeMap> {
     );
 
     // move camera
-    _mapController.animateCamera(
-      CameraUpdate.newCameraPosition(
-        CameraPosition(
-          target: LatLng(
-            _currentPosition.latitude,
-            _currentPosition.longitude,
+    if (_currentPositionCamera) {
+      _mapController.animateCamera(
+        CameraUpdate.newCameraPosition(
+          CameraPosition(
+            target: LatLng(
+              _currentPosition.latitude,
+              _currentPosition.longitude,
+            ),
+            zoom: 15.0,
           ),
-          zoom: 15.0,
         ),
-      ),
-    );
+      );
+    }
   }
 
   Future<String> _getGeolocationAddress(Position position) async {
